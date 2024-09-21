1d6e130c4b28580ef30648c6baf5510e27962d32
Giougt <alexandre18golf@gmail.com>
2024-09-22 01:15:49 +0200

    add title and decoration

--- site_js_unity/main.js
+++ site_js_unity/main.js
@@ -1,5 +1,6 @@
 /* enter data  */ 
 
+const title_section_1 = document.createTextNode("Calcule du pourcentage moyen d'une pente sur une distance");
 const text_meter = document.createTextNode("Distance (m)"); 
 const min_alt =  document.createTextNode("Minimum altitude (m)")
 const max_alt = document.createTextNode("Maximum altitude (m) ")
@@ -17,19 +18,26 @@ input_min_alt.id= "minAltitude";
 input_max_alt.id= "maxAltitude"; 
 
 /* button */
+
 const button_valid = document.createElement("button"); 
 button_valid.innerHTML= "Validate"; 
 
 /* special element  */ 
+
 const br = document.createElement("br"); 
 const br1 = document.createElement("br"); 
 
 /* create element in body */
+
+const title1 = document.createElement("h1");
 const div1 = document.createElement("div");
 const div2 = document.createElement("div");
 const div3 = document.createElement("div");
 
-/*  append const in div*/
+/*  append const(text) in div*/
+
+title1.appendChild(title_section_1);
+
 div1.appendChild(text_meter);
 div1.appendChild(input_meter); 
 
@@ -49,7 +57,13 @@ Object.assign(button_valid.style,{
     borderRadius: "10px"
 });
 
+Object.assign(title1.style,{
+    textDecoration : "underline"
+});
+
 /* append element in body */
+
+document.body.appendChild(title1);
 document.body.appendChild(div1); 
 document.body.appendChild(br);
 document.body.appendChild(div2); 
